d02985214d1764e011376fe4417298629d43361e
borenwu <borenwuapple@163.com>
2017-10-28 15:42:15 +0800

    third

--- server/api/pics.js
+++ server/api/pics.js
@@ -1,26 +1,42 @@
-import { Router } from 'express'
+import {Router} from 'express'
 const multer = require('multer');
 const bytes = require('bytes');
 const qn = require('qn');
+const fs = require('fs');
+const rd = require('rd');
 
 const router = Router()
 let bucket = 'ntboao'
 
+
 // 配置multer
 // 详情请见https://github.com/expressjs/multer
-const storage = multer.memoryStorage()
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: bytes('10MB') // 限制文件在10MB以内
+let createFolder = function (folder) {
+    try {
+        fs.accessSync(folder);
+    } catch (e) {
+        fs.mkdirSync(folder);
+    }
+};
+
+let uploadFolder = './tmp/my-uploads';
+
+createFolder(uploadFolder);
+
+// 通过 filename 属性定制
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
     },
-    fileFilter: function(req, files, callback) {
-        // 只允许上传jpg|png|jpeg|gif格式的文件
-        var type = '|' + files.mimetype.slice(files.mimetype.lastIndexOf('/') + 1) + '|';
-        var fileTypeValid = '|jpg|png|jpeg|gif|'.indexOf(type) !== -1;
-        callback(null, !!fileTypeValid);
+    filename: function (req, file, cb) {
+        // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+        cb(null, file.originalname);
     }
 });
+
+// 通过 storage 选项来对 上传行为 进行定制化
+const upload = multer({storage: storage})
+
 
 // 七牛云配置
 const client = qn.create({
@@ -35,42 +51,51 @@ const client = qn.create({
 })
 
 
-/* upload to qn */
-router.post('/pics/upload', function (req, res, next) {
-    // 上传单个文件
-    // 这里`image`对应前端form中input的name值
-    upload.single('file')(req, res, function(err) {
+router.post('/pics/upload', upload.single('file'), function (req, res, next) {
+    let file = req.file;
+    let dirname = req.body.dirname
+    console.log('dirname: %s', dirname)
+    let filename = file.originalname
+
+    console.log('文件类型：%s', file.mimetype);
+    console.log('原始文件名：%s', file.originalname);
+    console.log('文件大小：%s', file.size);
+    console.log('文件保存路径：%s', file.path);
+
+    client.uploadFile(file.path, {key: `/${dirname}/${filename}`}, function (err, result) {
         if (err) {
-            return console.error(err);
+            console.log('上传失败')
+            console.log(err)
+        } else {
+            let store_url = result.url;
+            console.log('result: %s', store_url)
+            // 上传之后删除本地文件
+            fs.unlinkSync(file.path);
         }
-        if (req.file && req.file.buffer) {
-            // 上传到七牛
-            let dirname = req.body.dirname
-            let filename = req.body.filename
 
-            let fbuffer = req.file.buffer
-            console.log(fbuffer.length)
-            console.log(fbuffer.indexOf('/0'))
-            // client.uploadFile(fbuffer.toString(0,fbuffer.length-1), {key: `/${dirname}/${filename}`}, function (err, result) {
-            //     if (err) {
-            //         console.log('上传失败')
-            //         console.log(err)
-            //     } else {
-            //         let store_url = result.url;
-            //         console.log('result: %s',store_url)
-            //     }
-            //     // 上传之后删除本地文件
-            //     //fs.unlinkSync(filePath);
-            // });
-
-            res.json({
-                msg:"1"
-            })
-        }
     });
 
-})
+    // // 异步遍历目录下的所有文件
+    // rd.eachSync(uploadFolder, function (f, s) {
+    //     if (s.size > 0) {
+    //         client.uploadFile(f, {key: `/${dirname}/${filename}`}, function (err, result) {
+    //             if (err) {
+    //                 console.log('上传失败')
+    //                 console.log(err)
+    //             } else {
+    //                 let store_url = result.url;
+    //                 console.log('result: %s',store_url)
+    //                 // 上传之后删除本地文件
+    //                 // fs.unlinkSync(f);
+    //             }
+    //
+    //         });
+    //     }
+    //     // 每找到一个文件都会调用一次此函数
+    //     // 参数s是通过 fs.stat() 获取到的文件属性值
+    // });
 
-
+    res.send({ret_code: '0'});
+});
 
 export default router
